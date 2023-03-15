977a6c458cdbafef608df50b9bea437b3949e8aa
1242550160@qq.com <1242550160@qq.com>
2023-03-16 00:03:22 +0800

    fix：修复

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from nonebot import load_plugins
+from nonebot import require, load_plugins
 
 dir_ = Path(__file__).parent
-load_plugins(str(dir_ / "nonebot_plugin_xiuxian_2.0"))
+require('nonebot_plugin_apscheduler')
+load_plugins(str(dir_ / "xiuxian"))
--- nonebot_plugin_xiuxian_2.0/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from nonebot import require, load_plugins
-
-dir_ = Path(__file__).parent
-require('nonebot_plugin_apscheduler')
-load_plugins(str(dir_ / "xiuxian"))
